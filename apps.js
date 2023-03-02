41fcd2068d3f3a8f99cfab5a6ca0fb7bacf25b4a
MariamiKvirkvia <121309330+MariamiKvirkvia@users.noreply.github.com>
2023-03-02 11:13:52 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -27,13 +27,15 @@ switch(currentDay){
         break;
 }
 
+
 // For Loop
 
 for(let i = 0; i <= 150; i++){
     console.log(i);
 }
 
-// While
+
+// While Loop
 
 let i = 0;
 while(i <= 100){
@@ -41,11 +43,93 @@ while(i <= 100){
     i++;
 }
 
-// Do While
+
+// Do While Loop
 
 i = 0;
 do{
     console.log(i);
     i++;
 }while(i <= 50);
+
+
+// Negative Result
+
+const currencies = [
+	{
+		code: "AED",
+		quantity: 10,
+		rateFormated: "7.1320",
+		diffFormated: "0.0209",
+		rate: 7.132,
+		name: "არაბეთის გაერთიანებული საამიროების დირჰამი",
+		diff: -0.0209,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+	{
+		code: "AMD",
+		quantity: 1000,
+		rateFormated: "6.7264",
+		diffFormated: "0.0014",
+		rate: 6.7264,
+		name: "სომხური დრამი",
+		diff: -0.0014,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+	{
+		code: "AZN",
+		quantity: 1,
+		rateFormated: "1.5409",
+		diffFormated: "0.0045",
+		rate: 1.5409,
+		name: "აზერბაიჯანული მანათი",
+		diff: -0.0045,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+	{
+		code: "CZK",
+		quantity: 10,
+		rateFormated: "1.1799",
+		diffFormated: "0.0055",
+		rate: 1.1799,
+		name: "ჩეხური კრონა",
+		diff: 0.0055,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+	{
+		code: "EUR",
+		quantity: 1,
+		rateFormated: "2.7791",
+		diffFormated: "0.0040",
+		rate: 2.7791,
+		name: "ევრო",
+		diff: 0.004,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+	{
+		code: "GBP",
+		quantity: 1,
+		rateFormated: "3.1674",
+		diffFormated: "0.0203",
+		rate: 3.1674,
+		name: "დიდი ბრიტანეთის გირვანქა სტერლინგი",
+		diff: 0.0203,
+		date: "2023-02-28T17:45:12.156Z",
+		validFromDate: "2023-03-01T00:00:00.000Z",
+	},
+];
+
+console.log(currencies[0].name);
+
+while(currencies.diff < 0){
+    console.log(currencies.name + currencies.diff);
+}
+
+
+
 
